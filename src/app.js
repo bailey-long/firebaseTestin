fd8010ccabd380ee902bf358be3be9aca4d524cb
Bailey Long <bailey-long@live.nmit.ac.nz>
2022-10-27 11:36:39 +1300

    added firebase to repo

---
+++ src/app.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA56aMLDnwrAuYQ85udKnmzXG1xOhZto-c",
+  authDomain: "aaaagh-c8514.firebaseapp.com",
+  projectId: "aaaagh-c8514",
+  storageBucket: "aaaagh-c8514.appspot.com",
+  messagingSenderId: "891645262722",
+  appId: "1:891645262722:web:130ff4399b1326f0cdad9b",
+  measurementId: "G-PWS4MERM4K"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
